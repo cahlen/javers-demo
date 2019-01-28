96a2dc44af873eb9d7478e4795867550368bceca
Cahlen Humphreys <cahlen@gmail.com>
2019-01-27 20:59:30 -0700

    Add initial barebones javers-demo project
    This project is unmodified from the spring initializr.

---
+++ javers-demo-app/src/main/java/com/example/javers/javersdemoapp/JaversDemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.javers.javersdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaversDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaversDemoAppApplication.class, args);
+    }
+
+}
+
